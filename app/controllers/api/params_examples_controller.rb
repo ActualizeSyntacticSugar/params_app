a19471e7c5b2bd7c32389a6d66755dc5378fc0f4
Peter Jang <peter.x.jang@gmail.com>
2019-11-06 18:47:03 -0600

    Add query params example action

--- app/controllers/api/params_examples_controller.rb
+++ app/controllers/api/params_examples_controller.rb
@@ -1,2 +1,7 @@
 class Api::ParamsExamplesController < ApplicationController
+  def query_params_method
+    @size = params["size"]
+    @sheep = params["sheep"]
+    render "query_params.json.jb"
+  end
 end
