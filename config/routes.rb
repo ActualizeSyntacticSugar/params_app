67c50b7be852bf9c0f308ca135e464c2829bfebc
Peter Jang <peter.x.jang@gmail.com>
2019-11-06 20:03:03 -0600

    Add query guess game example action

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     get "/query_params_url" => "params_examples#query_params_method"
 
     get "/name_url" => "games#name_method"
+    get "/query_guess_url" => "games#query_guess_method"
   end
 end
